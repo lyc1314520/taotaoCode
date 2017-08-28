c63987b262af2eb0af2f400913ca546f997cb359
lyc <liuyongchang0311@aliyun.com>
2017-08-28 19:52:37 +0800

    3

--- taotao/taotao-admin-web/src/main/java/com/taotao/admin/controller/PictrueController.java
+++ taotao/taotao-admin-web/src/main/java/com/taotao/admin/controller/PictrueController.java
@@ -25,32 +25,38 @@ import java.util.Map;
 @RestController
 public class PictrueController {
 
+    /** 注入FastDFS服务器的地址 */
     @Value("${fastdfsServerUrl}")
     private String fastdfsServerUrl;
-
-
-    @PostMapping(path = "/pic/upload", produces = MediaType.TEXT_HTML_VALUE)
+    /**
+     * 文件上传 (application/json --> text/html)
+     */
+    @PostMapping(path="/pic/upload", produces=MediaType.TEXT_HTML_VALUE)
     public String upload(@RequestParam("pic")MultipartFile multipartFile)throws Exception{
-        //定义响应数据
-        Map<String,Object> data = new HashMap<>();
-        data.put("error",500);
-        try {
-            //加载配置文件
-            String conf_filename = this.getClass().getResource("/fastdfs-client.conf").getPath();
-            //初始化客户端全局信息
+        /** 定义响应数据  {"error" : 0, "url" : ""} */
+        Map<String, Object> data = new HashMap<>();
+        data.put("error", 500);
+        try{
+            /** 加载配置文件fastdfs-client.conf */
+            String conf_filename= this.getClass()
+                    .getResource("/fastdfs-client.conf").getPath();
+            /** 初始化客户端全局信息 */
             ClientGlobal.init(conf_filename);
-            //创建储存客户端对象
+            /** 创建存储客户端对象 */
             StorageClient storageClient = new StorageClient();
-            //上传文件到FastDFS文件服务器
-            String[] arr = storageClient.upload_file(multipartFile.getBytes(), FilenameUtils.getExtension(multipartFile.getOriginalFilename()),null);
+            /** 上传文件到FastDFS文件服务器 */
+            String[] arr = storageClient.upload_file(multipartFile.getBytes(),
+                    FilenameUtils.getExtension(multipartFile.getOriginalFilename()),
+                    null);
+            // http://192.168.12.128 /group1/M00/00/00/xxx.jpg
             StringBuilder url = new StringBuilder(fastdfsServerUrl);
-            for (String str:arr) {
-                url.append("/"+str);
+            for (String str : arr){
+                url.append("/" + str);
             }
-            data.put("error",0);
-            data.put("url",url);
-        } catch (Exception e) {
-            data.put("error",500);
+            data.put("error", 0);
+            data.put("url", url);
+        }catch(Exception ex){
+            data.put("error", 500);
         }
         return new ObjectMapper().writeValueAsString(data);
     }
